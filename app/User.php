b1509c7a3c9fba339acf2d63d28bedcd0a6d77ce
Unknown <sabine.vdbroek1998@gmail.com>
2018-07-13 00:51:02 +0200

    Relationship model User

--- app/User.php
+++ app/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class, 'user_id', 'id');
+    }
 }
