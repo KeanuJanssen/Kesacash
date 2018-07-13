6149fea33bbe54c0caa4287890adc7c8a1614c4d
KeanuJanssen <38426534+KeanuJanssen@users.noreply.github.com>
2018-07-13 02:42:48 +0200

    create form

--- resources/views/transactions/create.blade.php
+++ resources/views/transactions/create.blade.php
@@ -14,7 +14,29 @@
                         </div>
                     @endif
 
-                    <form action="{{ route('transactions.create') }}" method="POST"></form>
+                    <form action="{{ route('transactions.create') }}" method="POST">
+                        <div class="form-group">
+                                <label for="name">name:</label>
+                                <input class="form-control" type="text" name="name" id="name" placeholder="Type name here" required>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="name">Description:</label>
+                                <textarea class="form-control" id="description" name="description" rows="3" placeholder="Type description here (not required)"></textarea>
+                            </div>
+
+                            <div class="form-group form-check">
+                                <input type="checkbox" class="form-check-input" id="negative" name="negative">
+                                <label class="form-check-label" for="negative">Negative amount</label>
+                            </div>
+                            
+                            <div class="form-group">
+                                <label for="name">Amount:</label>
+                                <input class="form-control" type="text" name="amount" id="amount" placeholder="Type amount here" required>
+                            </div>
+
+                            <button type="submit" class="btn btn-primary">Create transaction</button>
+                    </form>
                 </div>
             </div>
         </div>
